0815af56fc9c458cf5cd8b73a8ac5c320ddb1bb2
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-03-23 09:51:35 +0530

    useSearchParams

--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { closeMenu } from "../utils/sidebarSlice";
 
 const WatchVideo = () => {
-  return (
-    <div>WatchVideo</div>
-  )
-}
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
 
-export default WatchVideo
+  const [params] = useSearchParams();
+  console.log("id",params.get("v"));
+  return <div className="m-5">
+            <iframe
+            width="1200"
+            height="600"
+            src={"https://www.youtube.com/embed/" + params.get("v")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+   </div>;
+};
+
+export default WatchVideo;
--- src/utils/sidebarSlice.js
+++ src/utils/sidebarSlice.js
@@ -1,16 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const sidebarSlice=createSlice({
-    name:"navSlice",
-    initialState:{
-        isMenuOpen:true
+const sidebarSlice = createSlice({
+  name: "navSlice",
+  initialState: {
+    isMenuOpen: true,
+  },
+  reducers: {
+    toggleSidebar: (state) => {
+      state.isMenuOpen = !state.isMenuOpen;
     },
-    reducers:{
-        toggleSidebar:(state)=>{
-            state.isMenuOpen= !state.isMenuOpen;
-        }
-    }
-})
+    closeMenu: (state) => {
+      state.isMenuOpen = false;
+    },
+  },
+});
 
-export const {toggleSidebar}=sidebarSlice.actions;
+export const { toggleSidebar, closeMenu } = sidebarSlice.actions;
 export default sidebarSlice.reducer;
