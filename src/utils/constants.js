ea50e1e347a0233b5fbcb4b90e46489f455a25e7
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-03-24 11:04:58 +0530

    link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 
 const GOOGLE_API_KEY = "AIzaSyDS7HEVS2Um7sQvI-gNS3k2bxEiGPup_UY";
 
+//https://www.googleapis.com/youtube/v3/subscriptions?key=AIzaSyDS7HEVS2Um7sQvI-gNS3k2bxEiGPup_UY&part=snippet&channelId=kffacxfA7G4&maxResults=50
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
